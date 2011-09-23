dd5daae6d28806ccd410edbf8a840657d0992cb1
Reuben Peter-Paul <reuben.peterpaul@gmail.com>
2011-09-23 13:09:53 -0300

    regenrated html

--- github-page-ize.sh
+++ github-page-ize.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-HTML_DIR="./build/html"
-HAXFU="haxfu"
-CS6795="cs6795"
+BASE_DIR=$(pwd)
+HTML_DIR="$BASE_DIR/build/html"
+HAXFU="$HTML_DIR/haxfu"
+CS6795="$HTML_DIR/cs6795"
 
 cd $HTML_DIR
 mv _sources source
 mv _static static
 mv _images images
-sed -i 's/_images/images/g' *.html 
+sed -i 's/_images/images/g' *.html  #TODO: make procedure for these sed commands
 sed -i 's/_static/static/g' *.html 
 sed -i 's/_sources/sources/g' *.html
 mv main.html index.html
@@ -17,7 +18,7 @@ sed -i 's/_images/images/g' *.html
 sed -i 's/_static/static/g' *.html 
 sed -i 's/_sources/sources/g' *.html
 
-cd ../$CS6795
+cd $CS6795
 sed -i 's/_images/images/g' *.html 
 sed -i 's/_static/static/g' *.html 
 sed -i 's/_sources/sources/g' *.html
